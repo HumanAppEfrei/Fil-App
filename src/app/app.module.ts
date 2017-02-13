1c64656f648799dc093291df4f746c05ed9be567
WAWA <walid.mohand.amer@efrei.net>
2017-02-13 23:17:26 +0100

    First commit !

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,31 @@
+import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import {routing, appRoutingProviders} from './app.routing';
+import { AppComponent }  from './app.component';
+import {AUTH_PROVIDERS} from 'angular2-jwt';
 
-import { AppComponent } from './app.component';
+import {HomeComponent} from './components/home/home.component';
+import {ProfileComponent} from './components/profile/profile.component';
+
+import {PreventionComponent} from './components/prevention/prevention.component';
+import {DiagnostiqueComponent} from './components/diagnostique/diagnostique.component';
+import {TraitementComponent} from './components/traitement/traitement.component';
+import {RemissionComponent} from './components/remission/remission.component';
+
+import {Auth} from './services/auth.service';
+import {AuthGuard} from './auth.guard';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports: [ BrowserModule, routing ],
+  declarations: [ AppComponent, HomeComponent, ProfileComponent, RemissionComponent, DiagnostiqueComponent,
+  					TraitementComponent, PreventionComponent ],
+  bootstrap: [ AppComponent ],
+  providers: [
+    appRoutingProviders,
+    AUTH_PROVIDERS,
+    Auth,
+    AuthGuard
+  ]
 })
+
 export class AppModule { }
